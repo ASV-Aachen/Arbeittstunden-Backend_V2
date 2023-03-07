3291f7eec928fae66c965f21837cba5b53a11a4b
Christian Baltzer <35217578+Darker97@users.noreply.github.com>
2023-03-07 09:41:21 +0100

    added database handling

---
+++ setup/database.go
@@ -0,0 +1,43 @@
+package setup
+
+import (
+	"os"
+
+	db "github.com/ASV-Aachen/ArbeitsstundenDB/modules/DB"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// MariaDB
+var DB_USER string = os.Getenv("DB_USER")
+var DB_PASSWORD string = os.Getenv("DB_PASSWORD")
+var DB_NAME string = os.Getenv("DB_NAME")
+var DB_URL string = os.Getenv("DB_URL")
+
+var path string = "/usr/local/bin/sql/"
+
+func DB_Migrate(selectedDatabase *gorm.DB) {
+	// Migrate the schema
+	err := selectedDatabase.AutoMigrate(
+		&db.User{},
+		&db.Season{},
+		&db.Reduction{},
+		&db.Project{},
+		&db.Project_item{},
+		&db.Project_item_hour{},
+	)
+
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func SetUpMariaDB() *gorm.DB {
+	dsn := DB_USER + ":" + DB_PASSWORD + "@tcp(" + DB_URL + ":3306" + ")/" + DB_NAME
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
