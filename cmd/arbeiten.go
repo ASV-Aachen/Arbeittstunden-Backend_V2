c8ea0b003308cd1365be8fde89c19611be9d4865
Christian Baltzer <35217578+Darker97@users.noreply.github.com>
2023-03-07 09:40:49 +0100

    added work functions

---
+++ cmd/arbeiten.go
@@ -0,0 +1,115 @@
+package cmd
+
+import (
+	"github.com/ASV-Aachen/ArbeitsstundenDB/modules"
+	db "github.com/ASV-Aachen/ArbeitsstundenDB/modules/DB"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+func GetAllArbeitenForProject(c *fiber.Ctx, database *gorm.DB) error {
+	searched_id := c.Params("projectID")
+
+	var currentProject db.Project
+	database.Model(&db.Project{Id: searched_id}).First(currentProject)
+
+	var currentArbeiten db.Project_item
+	database.Model(&db.Project_item{Project: currentProject}).Find(currentArbeiten)
+
+	return c.Status(fiber.StatusOK).JSON(currentArbeiten)
+}
+
+func AddNewArbeitenForProject(c *fiber.Ctx, database *gorm.DB) error {
+	searched_id := c.Params("projectID")
+	var currentProject db.Project
+	database.Model(&db.Project{Id: searched_id}).First(currentProject)
+
+	json := new(modules.Json_Project_Item)
+	if err := c.BodyParser(json); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	var currentSeason db.Season
+	database.Model(&db.Season{Year: json.Season})
+
+	var temp db.Project_item
+	temp.Project = currentProject
+	temp.Season = currentSeason
+	temp.Date = json.Date
+	temp.Title = json.Title
+	temp.Description = json.Description
+	temp.Approved = json.Approved
+	temp.Countable = json.Countable
+
+	for _, i := range json.Work {
+		var temp_work db.Project_item_hour
+		temp_work.Duration = i.Duration
+
+		var currentUsers db.User
+		database.Model(&db.User{Id: i.Member}).First(currentUsers)
+		temp_work.Member = currentUsers
+
+		temp.Work = append(temp.Work, temp_work)
+	}
+
+	database.Create(temp)
+
+	return c.Status(fiber.StatusOK).SendStatus(200)
+}
+
+func EinzelneArbeitbearbeiten(c *fiber.Ctx, database *gorm.DB) error {
+	searched_id := c.Params("arbeitsID")
+	var currentArbeit db.Project_item
+	database.Model(&db.Project_item{Id: searched_id}).First(currentArbeit)
+
+	json := new(modules.Json_Project_Item)
+	if err := c.BodyParser(json); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	var currentSeason db.Season
+	database.Model(&db.Season{Year: json.Season})
+
+	currentArbeit.Season = currentSeason
+	currentArbeit.Date = json.Date
+	currentArbeit.Title = json.Title
+	currentArbeit.Description = json.Description
+	currentArbeit.Approved = json.Approved
+	currentArbeit.Countable = json.Countable
+
+	var tempArray []db.Project_item_hour
+
+	for _, i := range json.Work {
+		var temp_work db.Project_item_hour
+		temp_work.Duration = i.Duration
+
+		var currentUsers db.User
+		database.Model(&db.User{Id: i.Member}).First(currentUsers)
+		temp_work.Member = currentUsers
+
+		tempArray = append(tempArray, temp_work)
+	}
+
+	currentArbeit.Work = tempArray
+
+	database.Save(currentArbeit)
+
+	return c.Status(fiber.StatusOK).SendStatus(200)
+}
+
+func ArbeiteEntfernen(c *fiber.Ctx, database *gorm.DB) error {
+	searched_id := c.Params("arbeitsID")
+	var currentArbeit db.Project_item
+	database.Model(&db.Project_item{Id: searched_id}).First(currentArbeit)
+
+	database.Delete(currentArbeit)
+
+	return c.Status(fiber.StatusOK).SendStatus(200)
+}
+
+func GetAllArbeitenNotChecked(c *fiber.Ctx, database *gorm.DB) error {
+	var currentArbeit []db.Project_item
+	database.Model(&db.Project_item{Approved: false}).First(currentArbeit)
+
+	return c.Status(fiber.StatusOK).JSON(currentArbeit)
+}
