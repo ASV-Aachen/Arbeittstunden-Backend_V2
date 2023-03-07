d6968015af2c3c69dfe24ecfd8443832217bb046
Christian Baltzer <35217578+Darker97@users.noreply.github.com>
2023-03-07 09:40:55 +0100

    added member functions

---
+++ cmd/member.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"github.com/ASV-Aachen/ArbeitsstundenDB/modules"
+	db "github.com/ASV-Aachen/ArbeitsstundenDB/modules/DB"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+func GetAllMember(c *fiber.Ctx, database *gorm.DB) error {
+	var currentUsers []db.User
+	database.Model(&db.User{}).Find(currentUsers)
+
+	return c.Status(fiber.StatusOK).JSON(currentUsers)
+}
+
+func GetMember(c *fiber.Ctx, database *gorm.DB) error {
+	searched_id := c.Params("memberID")
+	var currentUsers db.User
+	database.Model(&db.User{Id: searched_id}).Find(currentUsers)
+
+	return c.Status(fiber.StatusOK).JSON(currentUsers)
+}
+
+func MemberBearbeiten(c *fiber.Ctx, database *gorm.DB) error {
+	searched_id := c.Params("memberID")
+	json := new(modules.JsonMemberRolle)
+	if err := c.BodyParser(json); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	var currentUsers db.User
+	database.Model(&db.User{Id: searched_id}).Find(currentUsers)
+	currentUsers.Rolle = json.Rolle
+	database.Save(currentUsers)
+
+	return c.SendString("DO MAGIC")
+}
